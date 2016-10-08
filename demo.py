517e6d3d239d90fe4fcaab338703e3c27d65051a
shazz <github@metaverse.fr>
2016-10-08 17:42:16 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,24 +1,67 @@
-from sklearn import tree
+#
+# Lesson #1 challenge from Siralogy - Introduction - Learn Python for Data Science #1
+# https://www.youtube.com/watch?v=T5pRlIbr6gg
+# 
+#
 
-clf = tree.DecisionTreeClassifier()
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
 
-## CHALLENGE - create 3 more classifiers...
-#1
-#2
-#3
+from sklearn.metrics import accuracy_score
 
-#[height, weight, shoe_size]
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
-     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+classifier_names = ["Decision Tree",
+         "K-Nearest Neighbors", 
+         "Linear Support Vector Machine", 
+         "Radial Basic Function Support Vector Machine", 
+         "Random Forest", 
+         "Neural Net", 
+         "AdaBoost",
+         "Naive Bayes"]
 
-Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
+classifier_classes = [
+    DecisionTreeClassifier(),
+    KNeighborsClassifier(3),
+    SVC(kernel="linear"),
+    SVC(kernel="rbf"),
+    RandomForestClassifier(),
+    MLPClassifier(alpha=1),
+    AdaBoostClassifier(),
+    GaussianNB()]
 
 
-#CHALLENGE - ...and train them on our data
-clf = clf.fit(X, Y)
+#training data [height, weight, shoe_size]
+X_train = [[181, 80, 44], [177, 70, 43], [160, 60, 38], 
+     [154, 54, 37], [166, 65, 40], [190, 90, 47], 
+     [175, 64, 39], [177, 70, 40], [159, 55, 37], 
+     [171, 75, 42], [181, 85, 43]]
 
-prediction = clf.predict([[190, 70, 43]])
+Y_train = ['male', 'male', 'female', 
+     'female', 'male', 'male', 
+     'female', 'female', 'female', 
+     'male', 'male']
 
-#CHALLENGE compare their reusults and print the best one!
+#testing data [height, weight, shoe_size]
+X_test = [[183, 82, 45], [189, 92, 47], [175, 70, 41], [155, 47, 38],
+          [144, 36, 36], [180, 60, 39], [195, 110, 45]]
+Y_test = ['male', 'male', 'male', 'female',
+          'female', 'female', 'male']
 
-print prediction
+# train and check the model against the test data for each classifier
+max_accuracy = 0
+for name, clf in zip(classifier_names, classifier_classes):     
+    clf = clf.fit(X_train, Y_train)
+    
+    # predict on test data
+    prediction = clf.predict(X_test)
+    accuracy = accuracy_score(Y_test, prediction)
+    if accuracy > max_accuracy:
+        best_classifier = name
+        max_accuracy = accuracy
+    
+    print("Using classifier", name, X_test, "are", prediction,"=> accuracy", accuracy)
+   
+print("The most accurate was", best_classifier,"(",max_accuracy*100,"% )")
