337977ec6767a366c5cd9b6161766d5b0fb9018a
shazz <github@metaverse.fr>
2016-10-09 19:22:21 -0400

    Generating training/testing data using model_selection

--- demo.py
+++ demo.py
@@ -14,51 +14,42 @@ from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 
-# accuracy scoring
+# accuracy scoring, and data partitioning
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-classifier_names = ["Decision Tree",
-         "K-Nearest Neighbors", 
-         "Linear Support Vector Machine", 
-         "Radial Basic Function Support Vector Machine", 
-         "Random Forest", 
-         "Neural Network", 
-         "AdaBoost",
-         "Gaussian Naive Bayes"]
-
-classifier_classes = [
-    DecisionTreeClassifier(),
-    KNeighborsClassifier(3),
-    SVC(kernel="linear"),
-    SVC(kernel="rbf"),
-    RandomForestClassifier(),
-    MLPClassifier(alpha=1),
-    AdaBoostClassifier(),
-    GaussianNB()]
-
-
-#training data [height, weight, shoe_size]
-X_train = [[181, 80, 44], [177, 70, 43], [160, 60, 38], 
+# Classifiers we'd like to compare
+classifiers = {
+        "DecisionTree": DecisionTreeClassifier(),
+        "K-Nearest Neighbors": KNeighborsClassifier(6),
+        "Linear Support Vector Machine":  SVC(kernel="linear"),
+        "Radial Basic Function Support Vector Machine": SVC(kernel="rbf"),
+        "Random Forest": RandomForestClassifier(n_estimators=50),
+        "Neural Network": MLPClassifier(alpha=1), 
+        "AdaBoost": AdaBoostClassifier(n_estimators=100),
+        "Gaussian Naive Bayes": GaussianNB()
+    }
+    
+# input data [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], 
      [154, 54, 37], [166, 65, 40], [190, 90, 47], 
      [175, 64, 39], [177, 70, 40], [159, 55, 37], 
      [171, 75, 42], [181, 85, 43]]
-
-Y_train = ['male', 'male', 'female', 
+# labels
+Y = ['male', 'male', 'female', 
      'female', 'male', 'male', 
      'female', 'female', 'female', 
      'male', 'male']
 
-#testing data [height, weight, shoe_size]
-X_test = [[183, 82, 45], [189, 92, 47], [175, 70, 41], [155, 47, 38],
-          [144, 36, 36], [180, 60, 39], [195, 110, 45]]
-Y_test = ['male', 'male', 'male', 'female',
-          'female', 'female', 'male']
+# Create training/testing datasets by sampling 'randomly' 80/20% of the input data 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # train and check the model against the test data for each classifier
 max_accuracy = 0
 best_classifiers = []
-for name, clf in zip(classifier_names, classifier_classes):     
-    clf = clf.fit(X_train, Y_train)
+for classifier in classifiers:
+    clf = classifiers[classifier]
+    clf.fit(X_train, Y_train)
     
     # predict on test data
     prediction = clf.predict(X_test)
@@ -67,11 +58,12 @@ for name, clf in zip(classifier_names, classifier_classes):
     
     # check if this is the best
     if accuracy > max_accuracy:
-        best_classifiers = [name]
+        best_classifiers = [classifier]
         max_accuracy = accuracy
     elif accuracy == max_accuracy:
-        best_classifiers.append(name)
+        best_classifiers.append(classifier)
     
-    print("Using classifier", name, X_test, "are", prediction,"=> accuracy", accuracy)
+    print("Using classifier", classifier, X_test, "are", prediction,"=> accuracy", accuracy)
    
 print("The most accurate classifiers are", best_classifiers,"with",max_accuracy*100,"% accuracy")
+
