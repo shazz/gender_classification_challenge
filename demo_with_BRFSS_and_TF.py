d6e50bf94ecfc06320df0ab9255ec3599343e925
shazz <github@metaverse.fr>
2016-10-25 18:43:25 -0400

    Fixed TF.learn labels issue

--- demo_with_BRFSS_and_TF.py
+++ demo_with_BRFSS_and_TF.py
@@ -24,9 +24,10 @@ from tensorflow.contrib import learn
 from tensorflow.contrib.learn import DNNClassifier
 from tensorflow.contrib.learn import LinearClassifier
 import importlib
-
+from monitor import ExampleMonitor
 
 tf.logging.set_verbosity(tf.logging.ERROR)
+example_monitor = ExampleMonitor()
 
 def getInstance(module, classname, params):
     my_module = importlib.import_module(module)
@@ -45,19 +46,20 @@ classifiers = {
     "Linear Support Vector Machine":                ["sklearn.svm", "SVC", {"kernel" : "linear"}],
     "Radial Basic Function Support Vector Machine": ["sklearn.svm", "SVC", { "kernel": "rbf" }],
     "Random Forest":                                ["sklearn.ensemble", "RandomForestClassifier", None],
-    "Neural Network":                               ["sklearn.neural_network", "MLPClassifier", None], 
+    "Neural Network":                               ["sklearn.neural_network", "MLPClassifier", {'activation': 'logistic', 'solver': 'adam', 'learning_rate': 'invscaling', 'momentum': .9,'nesterovs_momentum': True, 'learning_rate_init': 0.02}], 
     "AdaBoost":                                     ["sklearn.ensemble", "AdaBoostClassifier", None],
     "Gaussian Naive Bayes":                         ["sklearn.naive_bayes", "GaussianNB", None],
-    "TF Deep Neural Network":                       ["tensorflow.contrib.learn", "DNNClassifier", {"hidden_units": [5], "n_classes": 2, "enable_centered_bias": None}],
-    "TF Linear Classifier":                         ["tensorflow.contrib.learn", "LinearClassifier", {"n_classes": 2, "enable_centered_bias": None}]
+    "TF Deep Neural Network":                       ["tensorflow.contrib.learn", "DNNClassifier", {"hidden_units": [5], "n_classes": 2, "feature_columns": [tf.contrib.layers.real_valued_column("", dimension=2)], "enable_centered_bias": None}],
+    "TF Linear Classifier":                         ["tensorflow.contrib.learn", "LinearClassifier", {"n_classes": 2, "feature_columns": [tf.contrib.layers.real_valued_column("", dimension=2)], "enable_centered_bias": None}]
 } 
     
 # load data and convert Y into 1d vector    
-X, Y = data_importer.load_data(500)    
+X, Y = data_importer.load_data(2000)    
 print("X", X.shape, "Y", Y.shape)
-    
+print(Y)
+
 # train and check the model against the test data for each classifier
-iterations = 1
+iterations = 10
 results = {}
 for itr in range(iterations):   
     
@@ -65,23 +67,19 @@ for itr in range(iterations):
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)    
     
     for classifier in classifiers:
-        if classifier == "TF Deep Neural Network":
-            feature_columns = learn.infer_real_valued_columns_from_input(X_train)   
-            clf = DNNClassifier(feature_columns=feature_columns, 
-                               hidden_units=[10,10,10], 
-                               n_classes=2, enable_centered_bias=None);
-            clf.fit(X_train, Y_train, steps=200)
-        elif classifier == "TF Linear Classifier":
-            feature_columns = learn.infer_real_valued_columns_from_input(X_train)   
-            clf = LinearClassifier(n_classes=2, feature_columns=feature_columns)     
-            clf.fit(X_train, Y_train, steps=200)
+        clf = getInstance(classifiers[classifier][0], classifiers[classifier][1], classifiers[classifier][2])
+        if classifier == "TF Deep Neural Network" or classifier == "TF Linear Classifier": 
+            clf.fit(X_train, Y_train, max_steps=200)
         else:
-            clf = getInstance(classifiers[classifier][0], classifiers[classifier][1], classifiers[classifier][2])
             clf.fit(X_train, Y_train)
-        
-        
+            
         # predict on test data
         prediction = clf.predict(X_test)
+        
+        # work with sckit learn but not with TF.learn
+        #print("a person of 100 kg and 185 cm is probably a", "male" if clf.predict([[100.0, 185.0]]) == 1 else "female")
+        #print("a person of 55 kg and 158 cm is probably a", "male" if clf.predict([[55, 158]]) == 1 else "female")
+        #print("a person of 40 kg and 148 cm is probably a", "male" if clf.predict([[40, 148]]) == 1 else "female")
         
         # compute accuracy and sum it to the previous ones
         accuracy = accuracy_score(Y_test, prediction)
@@ -103,16 +101,18 @@ for v,k in halloffame:
 X, Y = data_importer.load_data(0)    
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)    
 
-#clf = classifiers[halloffame[0][1]]
-clf = getInstance(classifiers[halloffame[0][1]][0], classifiers[halloffame[0][1]][1], classifiers[halloffame[0][1]][2])
-clf.fit(X_train, Y_train)
-prediction = clf.predict(X_test)
+print("Retraining with best classifier", halloffame[0])
+bestclf = getInstance(classifiers[halloffame[0][1]][0], classifiers[halloffame[0][1]][1], classifiers[halloffame[0][1]][2])
+print("fit")
+bestclf.fit(X_train, Y_train)
+print("pred")
+prediction = bestclf.predict(X_test)
+print("accuracy")
 accuracy = accuracy_score(Y_test, prediction)
 
-print(clf, "accuracy", accuracy)
-print("a person of 100 kg and 185 cm is probably a", "male" if clf.predict([[100, 185]]) == 1 else "female")
-print("a person of 55 kg and 158 cm is probably a", "male" if clf.predict([[55, 158]]) == 1 else "female")
-print("a person of 40 kg and 148 cm is probably a", "male" if clf.predict([[40, 148]]) == 1 else "female")
-
+print(bestclf, "accuracy", accuracy)
+print("a person of 100 kg and 185 cm is probably a", "male" if bestclf.predict([[100, 185]]) == 0 else "female")
+print("a person of 55 kg and 158 cm is probably a", "male" if bestclf.predict([[55, 158]]) == 0 else "female")
+print("a person of 40 kg and 148 cm is probably a", "male" if bestclf.predict([[40, 148]]) == 0 else "female")
 
     
